7951318b2743f2232cdb644d929ba70967dd05db
Georgi Panayotov <smg@smg-bg.net>
2018-10-29 21:23:05 +0200

    Task 1 solution

---
+++ Task1/Car.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Task1
+{
+    /// <summary>
+    /// Class that describes Car 
+    /// with Brand, Model, VIN, Mileage, Price
+    /// </summary>
+    public class Car
+    {
+        private readonly string _vin;
+
+        public string Brand { get; set; }
+
+        public string Model { get; set; }
+
+        public string VIN { get { return _vin; } }
+
+        public uint Mileage { get; set; }
+        
+        public decimal Price { get; set; }
+
+        public string Message
+        {
+            get
+            {
+                return $"{Brand} ({Model}) - {Mileage} ({Price})";
+            }
+
+        }
+
+        public Car(string brand, string model, string vin, uint mileage, decimal price)
+        {
+            Brand = brand;
+            Model = model;
+            // we want to set the ONLY once in the constructor 
+            // and nowhere else;
+            _vin = vin;
+            Mileage = mileage;
+            Price = price;
+        }
+    }
+}
--- Task1/Program.cs
+++ Task1/Program.cs
@@ -6,6 +6,40 @@ namespace Task1
     {
         static void Main(string[] args)
         {
+            //1.Създайте клас Car, който описва марка, модел, пробег и цена. В главния метод(e.g.Main):
+            //    a.Декларирайте масив от коли
+            //    b.Прочетете цяло число N от потребителя
+            //    c.Добавете N на брой коли в масива
+            //    d.Изчистете екрана и изведете данните за колите в следния формат: Марка(Модел) - пробег(цена)
+
+
+            // a.
+            Car[] myCars;
+
+            // b.
+            Console.Write("How many cars do you have: ");
+            string input = Console.ReadLine();
+            int n = Convert.ToInt32(input);
+
+            // c. 
+            Random rnd = new Random();
+            myCars = new Car[n];
+            for (int i = 0; i < myCars.Length; i++)
+            {
+                myCars[i] = new Car(
+                        $"Brand Type {rnd.Next(1, 50)}",
+                        $"Model Type {rnd.Next(25, 50)}",
+                        $"VIN{rnd.Next(100000, 500000)}",
+                        (uint)rnd.Next(150000, 500000),
+                        Convert.ToDecimal(rnd.Next(10000, 150000))
+                    );
+            }
+
+            // d.
+            Console.Clear();
+
+            for (int i = 0; i < myCars.Length; i++)
+                Console.WriteLine(myCars[i].Message);
 
             Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
